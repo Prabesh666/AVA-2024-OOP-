5f8e18a7762304391d063396e8aa700ff6c22297
Prabesh Shah <115727555+Prabesh666@users.noreply.github.com>
2024-03-16 21:46:33 +0545

    cool

--- Punctuation1.java
+++ Punctuation1.java
@@ -1,4 +1,4 @@
-public class Punctuation {
+public class Punctuation1 {
 
     public static void main(String[] args) {
         String text = "Mary had a little lamb, her fleece was as white as snow, and everywhere Mary went, the lamb was sure to go. -that was a nice poem- the end. ";

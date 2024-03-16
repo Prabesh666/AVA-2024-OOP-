a7512e0da922647b1933c062878b41ea091a7128
Prabesh Shah <115727555+Prabesh666@users.noreply.github.com>
2024-03-16 21:21:48 +0545

    Personal Details

---
+++ PersonalDetails.java
@@ -0,0 +1,15 @@
+
+public class PersonalDetails {
+    public static void main(String args[]) {
+
+        String Name = "Prabesh Shah";
+        int age = 19;
+        
+        String address = "New Baneshwar";
+
+        System.out.println(Name);
+        System.out.println(age);
+        System.out.println(address);
+
+    }
+}
